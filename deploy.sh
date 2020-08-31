e91776d7ac53b3175737784e069d743ad1cc2128
SWP <switchaphon@SWPs-MacBook-Air.local>
2020-08-31 15:35:46 +0700

    Revised deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,17 @@
 #Build docker images (2 tags: latest & generated id)
-docker build -t switchaphon/multi-client:latest -t switchaphon/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t switchaphon/multi-server:latest -t switchaphon/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t switchaphon/multi-worker:latest -t switchaphon/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+#docker build -t switchaphon/multi-client:latest -t switchaphon/multi-client:$SHA -f ./client/Dockerfile ./client
+#docker build -t switchaphon/multi-server:latest -t switchaphon/multi-server:$SHA -f ./server/Dockerfile ./server
+#docker build -t switchaphon/multi-worker:latest -t switchaphon/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker build -t switchaphon/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t switchaphon/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t switchaphon/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 
 #Push docker images to docker hub
-docker push switchaphon/multi-client:latest
-docker push switchaphon/multi-server:latest
-docker push switchaphon/multi-worker:latest
+# docker push switchaphon/multi-client:latest
+# docker push switchaphon/multi-server:latest
+# docker push switchaphon/multi-worker:latest
 
 docker push switchaphon/multi-client:$SHA
 docker push switchaphon/multi-server:$SHA
